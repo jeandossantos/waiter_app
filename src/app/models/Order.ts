ecea3055a7e7430243a25115ef50e1eb8c387197
Jean F. dos Santos <jeanddg@hotmail.com>
2022-11-25 21:09:18 -0300

    ⚡️ create model order

---
+++ src/app/models/Order.ts
@@ -0,0 +1,45 @@
+import { model, Schema } from 'mongoose';
+import { IProduct } from './Product';
+
+export interface IOrder {
+  _id: string;
+  table: string;
+  status: 'WAITING' | 'IN_PROGRESS' | 'DONE';
+  product: IProduct;
+}
+
+export const Order = model(
+  'Order',
+  new Schema({
+    table: {
+      type: String,
+      required: true,
+    },
+    status: {
+      type: String,
+      enum: ['WAITING', 'IN_PROGRESS', 'DONE'],
+      default: 'WAITING',
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+    product: {
+      required: true,
+      type: [
+        {
+          product: {
+            type: Schema.Types.ObjectId,
+            ref: 'Product',
+            required: true,
+          },
+          quantity: {
+            type: Number,
+            required: true,
+            default: 1,
+          },
+        },
+      ],
+    },
+  })
+);
