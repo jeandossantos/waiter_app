859d5e6b0d862268e601ce201fe5ba313a7adecc
Jean F. dos Santos <jeanddg@hotmail.com>
2022-11-27 13:02:14 -0300

    ➕ adding helmet to application

--- src/app.ts
+++ src/app.ts
@@ -5,8 +5,10 @@ import express, { Request, Response, NextFunction } from 'express';
 import { routes } from './routes';
 
 import { CustomException } from './app/exceptions/CustomException';
+import helmet from 'helmet';
 
 const app = express();
+app.use(helmet());
 app.use(express.json());
 app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
 app.use(routes);
