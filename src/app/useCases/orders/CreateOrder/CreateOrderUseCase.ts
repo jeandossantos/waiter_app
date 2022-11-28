97191c63640b0c7bd63db5e572c36f4dc32a1497
Jean F. dos Santos <jeanddg@hotmail.com>
2022-11-28 14:07:35 -0300

    ✨ adding validation to create order use case

--- src/app/useCases/orders/CreateOrder/CreateOrderUseCase.ts
+++ src/app/useCases/orders/CreateOrder/CreateOrderUseCase.ts
@@ -1,5 +1,6 @@
 import { IOrderRepository } from '../../../interfaces/IOrderRepository';
 import { IOrder } from '../../../models/Order';
+import { z } from 'zod';
 
 export type CreateOrderUseCaseProps = Pick<IOrder, 'table' | 'status'> & {
   products: [{ product: string; quantity: number }];
@@ -8,7 +9,20 @@ export type CreateOrderUseCaseProps = Pick<IOrder, 'table' | 'status'> & {
 export class CreateOrderUseCase {
   constructor(private orderRepository: IOrderRepository) {}
 
-  async execute({ table, products, status }: CreateOrderUseCaseProps) {
+  async execute(props: CreateOrderUseCaseProps) {
+    const orderSchema = z.object({
+      table: z.string().min(1),
+      status: z.enum(['WAITING', 'IN_PROGRESS', 'DONE']).default('WAITING'),
+      products: z.tuple([
+        z.object({
+          product: z.string().min(5),
+          quantity: z.number().min(1),
+        }),
+      ]),
+    });
+
+    const { table, status, products } = orderSchema.parse(props);
+
     const order = await this.orderRepository.create({
       table,
       status,
